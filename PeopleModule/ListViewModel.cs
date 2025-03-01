6f2d336a977abf746cf1913f1b3d48d970faf852
Allan Ritchie <allan.ritchie@gmail.com>
2025-03-01 15:02:43 -0500

    Fix nav

--- PeopleModule/ListViewModel.cs
+++ PeopleModule/ListViewModel.cs
@@ -1,7 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SharedLib;
-using DetailNavCommand = VehicleModule.Contracts.DetailNavCommand;
 
 namespace PeopleModule;
 
